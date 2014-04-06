ae5ba39b1477acae38c57ad7571b71c3cd1e019e
Chae Jubb <cjubb39@gmail.com>
2014-04-06 01:37:22 -0400

    oh, look.  A thread table

--- threadtab.h
+++ threadtab.h
@@ -1,9 +1,13 @@
-#include "global_config.h"
-
 /*
  *	Holds information on threads
  *
  */
+
+#ifndef THREADTAB_H
+#define THREADTAB_H
+
+#include <stdint.h>
+#include "global_config.h"
 
 typedef struct threadtab_s threadtab;
 
@@ -11,22 +15,24 @@ typedef struct threadtab_s threadtab;
  *	Create thread_data object to be used in other functions
  *	Returns negative on error
  */
-struct thread_data create_thread_data(char *name, size_t length);
+struct thread_data *create_thread_data(char *name, uint64_t length);
 
 /*
  *	Create new thread table
  *	Returns the created thread table; NULL on error
  */
-threadtab threadtab_init();
+threadtab *threadtab_init();
 
 /*
  *	Insert thread data into thread table
  *	Returns negative on error
  */
-int threadtab_insert(threadtab thread_table, struct thread_data data);
+int threadtab_insert(threadtab *thread_table, struct thread_data *data);
 
 /*
  *	Destroy thread table
  *	Returns negative on error
  */
-int threadtab_destroy(threadtab thread_table);
+int threadtab_destroy(threadtab *thread_table);
+
+#endif

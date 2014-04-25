f260febc047b4d45d3f24bbd243a248a72c27c56
Chae Jubb <cjubb39@gmail.com>
2014-04-24 23:39:26 -0400

    FIXED (for real) header file consolidation

---
+++ mem_manage.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "include/mem_manage.h"
+#include "include/error_handling.h"
+
+/*
+ *	Check malloc return value of a string allocation
+ */
+char *malloc_checked_string(size_t size){
+	char *tmp = malloc(size * sizeof(char));
+	if (tmp == NULL)
+		die("Malloc Error. Exiting.");
+
+	return tmp;
+}
+
+heap_list_head* heap_list_init(heap_list_head *head){
+	head->head = NULL;
+	head->tail = NULL;
+
+	return head;
+}
+
+heap_list_head* heap_list_add(heap_list_head *head, void *data){
+	struct heap_list_node_s *tmp;
+	malloc_checked(tmp);
+	tmp->next = NULL;
+
+	if (head->head == NULL){
+		head->head = tmp;
+	} else {
+		head->tail->next = tmp;
+	}
+
+	head->tail = tmp;
+
+	return head;
+}
+
+heap_list_head* heap_list_purge(heap_list_head *head){
+	struct heap_list_node_s *cur = head->head, *next;
+
+	while(cur != NULL){
+		next = cur;
+		free(cur);
+		cur = next;
+	}
+
+	head->head = NULL;
+	head->tail = NULL;
+
+	return head;
+}

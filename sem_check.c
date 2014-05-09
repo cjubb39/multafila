893d93e1c4db4247490513e73fecb6e788dd661f
Aiden Yang <ayang@Aidens-MacBook-Pro.local>
2014-05-09 16:43:20 -0400

    function call sem check done

--- sem_check.c
+++ sem_check.c
@@ -38,37 +38,49 @@ void check_expr(ast *a){
 	// 
 }
 
+/* struct ast_list_s {
+	struct ast_s *data;
+	struct ast_list_s *next;
+};  */
+
 /*check args are the correct type. check func exists. */
-void check_func_call(ast *a){
+	void check_func_call(ast *a){
 	// check that function exists in the symbol table 
 	// get symbol name
-	symtab_entry *s = a->data.func_call.func_symtab;
-	char *name = s->name;
-	symtab_entry *lookup = symtab_search_scope(a->containing_scope, name);
-	if (lookup == NULL) {
-		printf("function used without being declared");
-	}
+		symtab_entry *s = a->data.func_call.func_symtab;
+		char *name = s->name;
+		symtab_entry *lookup = symtab_search_scope(a->containing_scope, name);
+		if (lookup == NULL) {
+			printf("function used without being declared");
+		}
 	// check that the arguments in the function call match the declared function
+		else {
+			ast_list *declaredargs = lookup->data.func_call.arguments;
+			ast_list *args = a->data.func_call.arguments;
 
-	ast *args = a->data.func_call.arguments;
-}
+			if (arglist_compare(declaredargs, args) != 1) {
+				printf("argument types do not match function declaration");
+			}
+
+		}
+	}
 
 /* binary node checker */
-void check_bin(ast *a){
-	ast *a left = a->data.bin.left;
-	ast *a right = a->data.bin.right;
-	symtab_entry *s1 = a->data.bin.left.symtab_ptr;
-	ast_type t1 = symtab_entry_get_type(s1);
-	
-	symtab_entry *s2 = a->data.bin.right.symtab_ptr;
-	ast_type t2 = symtab_entry_get_type(s2);
-	
+	void check_bin(ast *a){
+		ast *a left = a->data.bin.left;
+		ast *a right = a->data.bin.right;
+		symtab_entry *s1 = a->data.bin.left.symtab_ptr;
+		ast_type t1 = symtab_entry_get_type(s1);
+
+		symtab_entry *s2 = a->data.bin.right.symtab_ptr;
+		ast_type t2 = symtab_entry_get_type(s2);
+
 	/* if t2 is a AST_NULL type, check to see if it's another bin_node or func_call */
-	if( t2 == AST_NULL){
-		ast_node_type t2n = symtab_entry_get_node_type(s2);
-		if (t2n == AST_NODE_BIN){
-		check_bin(right);
-		} else if (t2n == AST_NODE_FUNCTION_CALL){
+		if( t2 == AST_NULL){
+			ast_node_type t2n = symtab_entry_get_node_type(s2);
+			if (t2n == AST_NODE_BIN){
+				check_bin(right);
+			} else if (t2n == AST_NODE_FUNCTION_CALL){
 				symtab_entry *s = a->data.func_call.func_symtab;
 				/* check func return type, see if it matches lvalue */
 				ast_type t3 = symtab_entry_get_type(s);
@@ -76,73 +88,89 @@ void check_bin(ast *a){
 					printf("return type does not match declaration"); 
 				}
 			}
+		}
+
+		if (are_equivalent(t1, t2) == 0){
+			printf("binary node error, not an integer");
+		} 
 	}
-	
-	if (are_equivalent(t1, t2) == 0){
-		printf("binary node error, not an integer");
-	} 
-}
 
 /* unary node checker */
-void check_unary(ast *a){
-	symtab_entry *s = a->data.unary.operand.symtab_ptr;
-	ast_type t = symbtab_entry_get_type(s);
-	
-	if( t != AST_INT){
-		printf("unary operator error, not an integer");
+	void check_unary(ast *a){
+		symtab_entry *s = a->data.unary.operand.symtab_ptr;
+		ast_type t = symbtab_entry_get_type(s);
+
+		if( t != AST_INT){
+			printf("unary operator error, not an integer");
+		}
 	}
-}
 
 /* while loop checker */
-void check_while(ast *a){
-	char *c = a->data.while_statement.conditional_statement.bin.op;
-	if( c != "==" || c != "!=" || c != ">" || c != "<" || c != ">=" || c != "<="){
-		printf("condition in while loop is not a boolean expression");
+	void check_while(ast *a){
+		char *c = a->data.while_statement.conditional_statement.bin.op;
+		if( c != "==" || c != "!=" || c != ">" || c != "<" || c != ">=" || c != "<="){
+			printf("condition in while loop is not a boolean expression");
 		}
-	ast *a while_body = a->data.while_statement.body;
-	check_stmt(while_body);
-}
+		ast *a while_body = a->data.while_statement.body;
+		check_stmt(while_body);
+	}
 
 
 /* if statement checker */
-void check_conditional(ast *a){
+	void check_conditional(ast *a){
 
 	/* should also check for funcs that return boolean??*/
-	char *c = a->data.conditional_statement.conditional_statement.bin.op;
-	if( c != "==" || c != "!=" || c != ">" || c != "<" || c != ">=" || c != "<="){
-		printf("condition in if statement is not a boolean expression");
+		char *c = a->data.conditional_statement.conditional_statement.bin.op;
+		if( c != "==" || c != "!=" || c != ">" || c != "<" || c != ">=" || c != "<="){
+			printf("condition in if statement is not a boolean expression");
 		}
 		
-	ast *a cond_stmt = a->data.conditional_statement.if_statement;
-	check_stmt(cond_stmt);
-	
-	ast *a else_stmt = a->data.conditional_statement.else_statement;
-	check_stmt(else_stmt);
-	
-}
+		ast *a cond_stmt = a->data.conditional_statement.if_statement;
+		check_stmt(cond_stmt);
+
+		ast *a else_stmt = a->data.conditional_statement.else_statement;
+		check_stmt(else_stmt);
+
+	}
 
 /* checks if a stmt node is valid */
-void check_stmt(ast *a){	
+	void check_stmt(ast *a){	
 	/* check to see if it belongs to one of the stmt types */
-	symtab_entry *s = a->data.stmt.body;
-	ast_type t = symbtab_entry_get_type(s);
-	if (t == AST_NULL){
-		ast_node_type t = symtab_entry_get_node_type(s);
-		switch(t){
-			case AST_NODE_BIN:
+		symtab_entry *s = a->data.stmt.body;
+		ast_type t = symbtab_entry_get_type(s);
+		if (t == AST_NULL){
+			ast_node_type t = symtab_entry_get_node_type(s);
+			switch(t){
+				case AST_NODE_BIN:
 				check_bin;
-			case AST_NODE_FUNCTION_CALL:
+				case AST_NODE_FUNCTION_CALL:
+			}
 		}
 	}
-}
 
+	int arglist_compare(ast_list a, ast_list b) {
+		while(1)
+		{
+			if(a == NULL && b == NULL)     
+				{  return 1; }
+			if(a == NULL && b != NULL)  
+				{  return 0; }
+			if(a != NULL && b == NULL)  
+				{  return 0; }
+			if(a->data != b->data)
+				{  return 0; }
+			a = a->next;
+			b = b->next; 
+		}          
+
+	}
 /* check the main ast */
-void check_ast(ast *){
-	if (a == NULL){
-		return;
+	void check_ast(ast *a){
+		if (a == NULL){
+			return;
+		}
+
+		switch(ast_get_node_type(a)){
+			case 
+		}
 	}
-	
-	switch(ast_get_node_type(a)){
-		case 
-	}
-}
